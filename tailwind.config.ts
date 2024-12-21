452d645a2f8df1f8706459ff1b367cd778fc41f7
aditya <aditya.misra@u.nus.edu>
2024-12-21 17:31:15 +0530

    Set up initial home page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,41 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        "fade-in": "fade-in 0.5s ease-in-out forwards",
+        "title": "title 2s ease-in forwards"
+      },
+      keyframes: {
+        "fade-in": {
+          '0%': {
+            opacity: '0%'
+          },
+          '75%': {
+            opacity: '0%'
+          },
+          '100%': {
+            opacity: '100%'
+          }
+        },
+        "title": {
+          '0%': {
+            'line-height': '0%',
+            'letter-spacing': '0.25em',
+            opacity: '0'
+          },
+          '25%': {
+            'line-height': '0%',
+            opacity: '0%'
+          },
+          '80%': {
+            opacity: '100%'
+          },
+          '100%': {
+            'line-height': '100%',
+            opacity: '100%'
+          }
+        }
+      }
     },
   },
   plugins: [],
