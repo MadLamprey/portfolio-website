1a5dafe1b8c6c90e48b24ce78f3acbc645aabecd
aditya <aditya.misra@u.nus.edu>
2025-01-05 14:11:13 +0530

    Add skills to about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import NavBar from "../components/navbar"
-import { University, Library, Pencil, LinkIcon } from "lucide-react";
+import { University, Library, Pencil, LinkIcon, Computer } from "lucide-react";
 import { useState } from "react";
 import {Popover, PopoverTrigger, PopoverContent} from "@nextui-org/popover";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Education() {
     const [isEduOpen, setIsEduOpen] = useState(false);
+    const [isSkillOpen, setIsSkillOpen] = useState(false);
     const [isBlogOpen, setIsBlogOpen] = useState(false);
     const [isReadOpen, setIsReadOpen] = useState(false);
 
@@ -32,7 +34,7 @@ export default function Education() {
     return (
         <div className="flex flex-col items-center justify-center mx-auto gap-15">
             <NavBar />
-            <div className={`my-40 flex flex-col items-center justify-center gap-20 ${isEduOpen || isBlogOpen || isReadOpen ? "blur": ""}`}>
+            <div className={`my-40 flex flex-col items-center justify-center gap-20 ${isEduOpen || isSkillOpen || isBlogOpen || isReadOpen ? "blur": ""}`}>
                 <div className="text-zinc-200 text-6xl animate-title text-center">Hi!</div>
                 <div className="border-4 rounded-xl border-zinc-500 p-10 max-w-6xl z-10 text-zinc-200">
                     <p className="text-center">I am a <span className="text-2xl">Computer Science</span> student from India, currently studying in Singapore. My coursework and internships have revealed the transformative power of <span className="text-2xl">software engineering</span>, inspiring me to build solutions for <span className="text-2xl">social good</span>. I am also deeply intrigued by <span className="text-2xl">Artificial Intelligence</span> and <span className="text-2xl">Machine Learning</span>, and how they will shape our future.</p>
@@ -53,6 +55,41 @@ export default function Education() {
                             <p className="text-lg">2022 - 2026 (Expected)</p>
                             <br />
                             <p className="text-md">Specializing in Software Engineering, Database Systems and Artifical Intelligence & Machine Learning </p>
+                        </div>
+                    </PopoverContent>
+                </Popover>
+                <Popover placement="top" isOpen={isSkillOpen} onOpenChange={(x) => setIsSkillOpen(x)}>
+                    <PopoverTrigger>
+                        <div className="border rounded-xl border-zinc-600 hover:bg-zinc-600 flex flex-col items-center justify-center w-56 h-24 hover:cursor-pointer" onClick={() => {setIsSkillOpen(true)}}>
+                            <span className="text-zinc-200 border-zinc-500">{<Computer size={30} />}</span>
+                            <div className="text-zinc-200 text-2xl">Skills</div>
+                        </div>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <div className="border-4 rounded-xl border-zinc-600 p-10 max-w-xl text-zinc-200 text-center flex flex-col gap-5">
+                            <span className="text-xl">Languages</span>
+                            <div className="flex flex-row gap-5 items-center justify-center">
+                                <Image src="/skills/python.png" alt="Python" width={40} height={40} />
+                                <Image src="/skills/java.png" alt="Java" width={40} height={40} />
+                                <Image src="/skills/c.png" alt="C" width={40} height={40} />
+                                <Image src="/skills/js.png" alt="JavaScript" width={40} height={40} />
+                                <Image src="/skills/ts.png" alt="TypeScript" width={40} height={40} />
+                                <Image src="/skills/sql.png" alt="SQL" width={40} height={40} />
+                            </div>
+                            <span className="text-xl">Frameworks</span>
+                            <div className="flex flex-row gap-5 items-center justify-center">
+                                <Image src="/skills/pytorch.png" alt="PyTorch" width={40} height={40} />
+                                <Image src="/skills/angular.png" alt="Angular" width={40} height={40} />
+                                <Image src="/skills/react.png" alt="React" width={40} height={40} />
+                                <Image src="/skills/fastapi.png" alt="FastAPI" width={40} height={40} />
+                            </div>
+                            <span className="text-xl">Tools</span>
+                            <div className="flex flex-row gap-5 items-center justify-center">
+                                <Image src="/skills/docker.png" alt="Docker" width={40} height={40} />
+                                <Image src="/skills/git.png" alt="Git" width={40} height={40} />
+                                <Image src="/skills/postman.png" alt="Postman" width={40} height={40} />
+                                <Image src="/skills/vim.png" alt="Vim" width={40} height={40} />
+                            </div>
                         </div>
                     </PopoverContent>
                 </Popover>
